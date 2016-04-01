4da43edf63f33b843e435e1eba8a1c9ca9cc1ca2
IPutuAdi <IPutuAdi@Prahasta-Giri>
2016-04-01 22:04:05 +0700

    Merge origin/master
    Conflicts:
    	BiroPerjalanan/src/Orang.java
    	BiroPerjalanan/src/Pelanggan.java
    	BiroPerjalanan/src/Perjalanan.java
    	BiroPerjalanan/src/TempatWisata.java
    	BiroPerjalanan/src/biroperjalanan/Orang.java
    	BiroPerjalanan/src/biroperjalanan/PaketWisata.java
    	BiroPerjalanan/src/biroperjalanan/Pelanggan.java
    	BiroPerjalanan/src/biroperjalanan/Perjalanan.java
    	BiroPerjalanan/src/biroperjalanan/Petugas.java
    	BiroPerjalanan/src/biroperjalanan/TempatWisata.java

--- BiroPerjalanan/src/AplikasiKonsol.java
+++ BiroPerjalanan/src/AplikasiKonsol.java
@@ -1,6 +1,5 @@
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -12,43 +11,116 @@ import java.util.Scanner;
  *
  * @author anuge
  */
+import java.lang.*;
+import java.util.Scanner;
 public class AplikasiKonsol {
-    private ArrayList<TempatWisata> daftarTujuanWisata = new ArrayList<>();
-    private ArrayList<Pelanggan> pelanggan = new ArrayList<>();
-    private ArrayList<Petugas> petugas = new ArrayList<>();
-    private ArrayList<Perjalanan> perjalanan = new ArrayList<>();
-    private ArrayList<PaketWisata> daftarPaketWisata = new ArrayList<>();
-    private ArrayList<TempatWisata> daftarTempatWisata = new ArrayList<>();
-    
-    
-    
-    public static void main(String[] args){
-        Scanner scan = new Scanner (System.in);
-        int menu;
-        int admin;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        Perjalanan p = new Perjalanan();
+        PaketWisata pw = new PaketWisata();
         
+        TempatWisata tw1 = new TempatWisata();
+        pw.addDaftarTujuanWisata(tw1);
+        
+        
+        Petugas pet = new Petugas("","","","");
+        
+	Pelanggan pel1 = new Pelanggan("","","");
+	pel1.setKota("");
+        p.addPelanggan(pel1);
+        p.setPaket(pw);
+        p.setHarga(50000);
+
+        /*Pelanggan pel2 = new Pelanggan("Dwiki","C755FA", "081559955105");
+	pel2.setKota("Jakarta");
+        p.addPelanggan(pel2);
+        p.setPaket(pw);
+        p.setHarga(70000);
+                 System.out.println("Nama                : "+p.getPelangganId("C755FA").getNama());
+                 System.out.println("ID                  : "+p.getPelangganId("C755FA").getIdPelanggan());
+                 System.out.println("Kota                : "+p.getPelangganId("C755FA").getKota());
+                 System.out.println("No. HP              : "+p.getPelangganId("C755FA").getNoHP());
+                 System.out.println("Harga Perjalanan    : "+p.getTotalHarga("A305B"));*/
+        int x=0;
+        do{
         System.out.println("SISTEM INFORMASI BIRO PERJALANAN");
         System.out.println("===========PILIH MENU===========");
-        System.out.println("1. Admin");
-        System.out.println("2. Petugas");
+        System.out.println("1. Input Data Petugas");
+        System.out.println("2. Input Data Pelanggan");
+        System.out.println("3. Display Petugas");
+        System.out.println("4. Display Pelanggan 1");
+        System.out.println("5. Keluar");
+        
+        Scanner scan = new Scanner (System.in);
         System.out.println("Pilih menu ke: ");
-        menu=scan.nextInt();
-        switch(menu){
+        x=scan.nextInt();
+        switch(x){
             case 1:
-                System.out.println("SISTEM INFORMASI BIRO PERJALANAN");
-                System.out.println("===========MENU ADMIN===========");
-                System.out.println("1. Tambah Petugas");
-                System.out.println("2. Tambah Pelanggan");
-                System.out.println("Pilih menu ke: ");
-                admin=scan.nextInt();
-                
-                switch(admin){
-                    case 1:
-                        
-                    case 2:
-                        
-                }    
+                System.out.println("----Input Data Petugas----");
+                System.out.print("Masukkan Nama : ");
+                String nama = scan.next();
+                pet.setNama(nama);
+                System.out.print("Masukkan ID : ");
+                String idPetugas  = scan.next();
+                pet.setIdPetugas(idPetugas);
+                System.out.print("Masukkan No HP : ");
+                String noHP = scan.next();
+                pet.setNoHP(noHP);
+                System.out.print("Masukkan Alamat : "+pet.getAlamat());
+                String alamat = scan.next();
+                pet.setAlamat(alamat);
+                break;
+            
             case 2:
-        }    
-    }
+                System.out.println("----Input Data Pelanggan----");
+                
+                System.out.print("Masukkan Nama : ");
+                String name = scan.next();
+                pel1.setNama(name);
+                
+                System.out.print("Masukkan ID : ");
+                String idPelanggan = scan.next();
+                pel1.setIdPelanggan(idPelanggan);
+                
+                System.out.println("Masukkan No HP:  ");
+                String hp = scan.next();
+                pel1.setNoHP(hp);
+                
+                System.out.println("Masukkan kota : ");
+                String kota = scan.next();
+                pel1.setKota(kota);
+                
+                System.out.println("Masukkan Nama Tempat Wisata : ");
+                String tempat = scan.next();
+                tw1.setNamaTempat(tempat);
+                
+                System.out.println("Masukkan Nama Daerah : ");
+                String daerah = scan.next();
+                tw1.setNamaDaerah(daerah);
+                
+                System.out.println("Masukkan Harga : ");
+                double harga = scan.nextDouble();
+                tw1.setHarga(harga);
+                
+                System.out.println("Masukkan Id Tempat Wisata : ");
+                String idWst = scan.next();
+                tw1.setHarga(harga);
+                break;
+            case 3:
+                pet.displayInfo();
+                break;
+            case 4:
+                pel1.displayInfo();
+                tw1.displayInfo();
+                break;
+            case 5:
+                System.exit(0);
+        
+        }
+        }while (x!=9);
+        }
 }
